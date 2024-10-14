c60eb783e25d29780ac2643f1536f352dbcdb7f0
menisadi <menisadi@gmail.com>
2024-10-14 08:26:21 +0300

    added setup

--- lua/justaprint/init.lua
+++ lua/justaprint/init.lua
@@ -5,7 +5,14 @@ function M.hello()
 	print("Hello, Neovim!")
 end
 
--- Create a command that calls the hello function
-vim.api.nvim_create_user_command("PrintHello", M.hello, {})
+function M.setup()
+	print("Hello, Neovim!")
+
+	-- Create a command that calls the hello function
+	vim.api.nvim_create_user_command("PrintHello", M.hello, {})
+
+	-- Create a keymap that calls the hello function
+	vim.api.nvim_set_keymap("n", "<leader>ph", "<cmd>PrintHello<cr>", { noremap = true, silent = true })
+end
 
 return M
